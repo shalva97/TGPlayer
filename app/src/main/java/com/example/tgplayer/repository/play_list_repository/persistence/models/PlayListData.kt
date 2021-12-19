c68d3875ce8def1da3e6d3eca1277d30b1173e1c
shalva97 <shalva258@gmail.com>
2021-12-19 20:20:29 +0400

    add selected property

--- app/src/main/java/com/example/tgplayer/repository/play_list_repository/persistence/models/PlayListData.kt
+++ app/src/main/java/com/example/tgplayer/repository/play_list_repository/persistence/models/PlayListData.kt
@@ -1,6 +1,7 @@
 package com.example.tgplayer.repository.play_list_repository.persistence.models
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity
@@ -8,4 +9,6 @@ data class PlayListData(
     @PrimaryKey
     val playListID: String,
     val color: Int,
+    @Ignore
+    var selected: Boolean,
 )
