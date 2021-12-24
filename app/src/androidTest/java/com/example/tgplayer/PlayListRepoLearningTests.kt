8006261bb0db126b11a5f3408539888c15fffd7e
shalva97 <shalva258@gmail.com>
2021-12-24 21:07:42 +0400

    remove database callback

--- app/src/androidTest/java/com/example/tgplayer/PlayListRepoLearningTests.kt
+++ app/src/androidTest/java/com/example/tgplayer/PlayListRepoLearningTests.kt
@@ -2,8 +2,6 @@ package com.example.tgplayer
 
 import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.tgplayer.repository.play_list_repository.persistence.PlayListDao
@@ -16,7 +14,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-//@Ignore("we do not need to run learning tests")
 class PlayListRepoLearningTests {
 
     private lateinit var playListDao: PlayListDao
@@ -26,28 +23,36 @@ class PlayListRepoLearningTests {
     fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
-            context, PlayListDatabase::class.java)
-            .addCallback(object : RoomDatabase.Callback() {
-                override fun onCreate(db: SupportSQLiteDatabase) {
-                    super.onCreate(db)
-                    db.beginTransaction()
-                    db.execSQL("INSERT INTO PlayListData (playListID, color) VALUES ('All', 1234)")
-                    db.endTransaction()
-                }
-            })
-            .build()
+            context, PlayListDatabase::class.java).build()
         playListDao = db.playlistDao()
-    }
-
-    @Test
-    fun databaseIsPrePopulatedByOnePlaylist() {
-        val data = playListDao.getPlayLists()
-
-        assert(data.size == 1)
     }
 
     @Test()
     fun doStuff() {
+
+        val playlist = listOf(
+            PlayListData("one", 123),
+            PlayListData("two", 123),
+            PlayListData("three", 123),
+            PlayListData("blah", 123),
+        )
+        playListDao.addPlayList(
+            playlist
+        )
+
+        playListDao.addAudio(audios)
+        playListDao.addAudioToPlayList(PlayListAudioCrossRef("one", 1))
+        playListDao.addAudioToPlayList(PlayListAudioCrossRef("one", 2))
+        playListDao.addAudioToPlayList(PlayListAudioCrossRef("one", 3))
+
+        playListDao.addAudioToPlayList(PlayListAudioCrossRef("two", 2))
+        playListDao.addAudioToPlayList(PlayListAudioCrossRef("blah", 2))
+
+        val playLists = playListDao.getPlayLists()
+        123
+    }
+
+    companion object {
         val audios = listOf(
             Audio(
                 name = "Doom Soundtrack Metal Cover",
@@ -80,26 +85,5 @@ class PlayListRepoLearningTests {
                 length = 22
             ),
         )
-        val playlist = listOf(
-            PlayListData("one", 123),
-            PlayListData("two", 123),
-            PlayListData("three", 123),
-            PlayListData("blah", 123),
-        )
-        playListDao.addPlayList(
-            playlist
-        )
-
-        playListDao.addAudio(audios)
-        playListDao.addAudioToPlayList(PlayListAudioCrossRef("one", 1))
-        playListDao.addAudioToPlayList(PlayListAudioCrossRef("one", 2))
-        playListDao.addAudioToPlayList(PlayListAudioCrossRef("one", 3))
-
-        playListDao.addAudioToPlayList(PlayListAudioCrossRef("two", 2))
-        playListDao.addAudioToPlayList(PlayListAudioCrossRef("blah", 2))
-
-        val playLists = playListDao.getPlayLists()
-        123
     }
-
 }
--- app/src/main/java/com/example/tgplayer/di/PersistenceModule.kt
+++ app/src/main/java/com/example/tgplayer/di/PersistenceModule.kt
@@ -2,8 +2,6 @@ package com.example.tgplayer.di
 
 import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.tgplayer.repository.play_list_repository.persistence.PlayListDao
 import com.example.tgplayer.repository.play_list_repository.persistence.PlayListDatabase
 import dagger.Module
@@ -20,15 +18,8 @@ class PersistenceModule {
     @Provides
     @Singleton
     fun room(@ApplicationContext context: Context): PlayListDao {
-        return Room.databaseBuilder(context, PlayListDatabase::class.java, "playlist.db")
-            .addCallback(object : RoomDatabase.Callback() {
-                override fun onCreate(db: SupportSQLiteDatabase) {
-                    super.onCreate(db)
-                    db.beginTransaction()
-                    db.execSQL("INSERT INTO PlayListData (playListID, color) VALUES ('All', 1234)")
-                    db.endTransaction()
-                }
-            })
+        return Room
+            .databaseBuilder(context, PlayListDatabase::class.java, "playlist.db")
             .build()
             .playlistDao()
     }
