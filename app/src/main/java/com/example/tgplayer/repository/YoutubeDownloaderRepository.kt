e8ab7bf137789f78d75e9d281ab045d99827e796
Temur_Khatiashvili <temurkhatiashvili@gmail.com>
2021-12-25 18:52:30 +0400

    all news

--- app/src/main/java/com/example/tgplayer/repository/YoutubeDownloaderRepository.kt
+++ app/src/main/java/com/example/tgplayer/repository/YoutubeDownloaderRepository.kt
@@ -1,37 +1,87 @@
 package com.example.tgplayer.repository
 
-import android.net.Uri
+import android.content.Context
+import android.media.MediaRecorder
+import android.os.Environment
 import com.example.tgplayer.model.Audio
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import org.schabi.newpipe.extractor.services.youtube.YoutubeService
-import org.schabi.newpipe.extractor.stream.AudioStream
 import org.schabi.newpipe.extractor.stream.StreamExtractor
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 import javax.inject.Inject
 import javax.inject.Singleton
 
+typealias RoomAudio = com.example.tgplayer.repository.play_list_repository.persistence.models.Audio
+
+
 @Singleton
 class YoutubeDownloaderRepository @Inject constructor(
+    @ApplicationContext
+    private val context: Context,
     private val youtubeService: YoutubeService,
+    private val okHttpClient: OkHttpClient
 ) {
 
+    @Throws(IOException::class)
+    private fun downloadAudio(uri: String): ByteArray? {
+        lateinit var response: Response
+        val request = Request.Builder()
+            .url(uri)
+            .build()
+        response = okHttpClient.newCall(request).execute()
+        return response.body?.bytes()
 
-
-     fun getAudioForTest(url: String): StreamExtractor {
-        val video = youtubeService.getStreamExtractor(url)
-
-         video.fetchPage()
-
-
-
-
-
-         return video
     }
 
-    fun getAudio(url: String):Audio{
+    private fun saveAudio(filePath: String, byteArray: ByteArray?) {
+        context.openFileOutput(filePath, Context.MODE_PRIVATE).use { output ->
+            output.write(byteArray)
+        }
+
+    }
+
+
+    fun saveFileEndReturnAudioWithFileManagerPath(audio: Audio): RoomAudio {
+        val byteArray = downloadAudio(audio.audioSource)
+        saveAudio(audio.name, byteArray)
+        return RoomAudio(
+            name = audio.name,
+            pathOnDevice = audio.name,
+            url = audio.audioSource,
+            thumbnailPath = audio.thumbnail,
+            length = audio.length
+        )
+    }
+
+    fun getAudioFromFile(filePath: String){
+        context.openFileInput(filePath).use { stream ->
+            val audio = stream.bufferedReader().use {
+                it.readText()
+            }
+        }
+    }
+
+
+    fun getAudioForTest(url: String): StreamExtractor {
+        val video = youtubeService.getStreamExtractor(url)
+
+        video.fetchPage()
+
+
+        return video
+    }
+
+    fun getAudio(url: String): Audio {
         val video = youtubeService.getStreamExtractor(url)
         video.fetchPage()
 
-        return  Audio(
+        return Audio(
             audioSource = video.audioStreams.sortedBy { it.bitrate }.last().url,
             name = video.name,
             thumbnail = video.thumbnailUrl,
--- app/src/main/java/com/example/tgplayer/repository/play_list_repository/PlayListRepository.kt
+++ app/src/main/java/com/example/tgplayer/repository/play_list_repository/PlayListRepository.kt
@@ -2,6 +2,7 @@ package com.example.tgplayer.repository.play_list_repository
 
 import com.example.tgplayer.model.Audio
 import com.example.tgplayer.model.PlayList
+import com.example.tgplayer.repository.RoomAudio
 import com.example.tgplayer.repository.play_list_repository.persistence.PlayListDao
 import javax.inject.Inject
 
@@ -16,6 +17,10 @@ class PlayListRepository @Inject constructor(
     fun getAllAudio(): List<Audio> {
         return playListDao.getAllAudio()
             .map { audio -> audio.toPresentationModel() }
+    }
+
+    fun addAudio(audio: RoomAudio){
+        playListDao.addAudio(audio)
     }
 
 }
