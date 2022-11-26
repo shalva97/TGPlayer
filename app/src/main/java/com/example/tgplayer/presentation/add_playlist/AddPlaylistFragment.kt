07d3076ed5d27552b797f109051fede635b26490
George Shalvashvili <shalva258@gmail.com>
2022-11-26 17:29:12 +0400

    added new playlist fragment

---
+++ app/src/main/java/com/example/tgplayer/presentation/add_playlist/AddPlaylistFragment.kt
@@ -0,0 +1,13 @@
+package com.example.tgplayer.presentation.add_playlist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.tgplayer.R
+
+class AddPlaylistFragment : Fragment(R.layout.add_playlist) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
