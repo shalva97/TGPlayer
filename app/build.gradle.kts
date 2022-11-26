e1b831f302d7931ee5754e6eaaa5bbbd3bab56e0
George Shalvashvili <shalva258@gmail.com>
2022-11-26 16:09:36 +0400

    add tabs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.8.0-alpha03")
+    implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     val fragment_version = "1.5.4"
     implementation("androidx.fragment:fragment-ktx:$fragment_version")
